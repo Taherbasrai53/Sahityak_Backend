7b3acd7a36b0338b271efde00273f81eff359838
husainali72 <taherbasrai5353@gmail.com>
2023-08-02 22:14:09 +0530

    Innitial Commit

---
+++ Sahityak/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sahityak.Models;
+
+namespace Sahityak.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Sahityak/Models/User.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sahityak.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }        
+        public int? SahityakId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        public string? LastName { get; set; }
+        [Required]
+        public DateTime DOB { get; set; }
+        [Required]
+        public string Institution { get; set; }
+        public int? CollegeId { get; set; }
+        [Required]
+        public string Mob_Num { get; set; }
+        public string? Whatsapp_Num { get; set; }
+        [Required]
+        public string EmailId { get; set; }
+        public EUserType UserType { get; set; } = EUserType.User;
+        [Required]
+        public string Password { get; set; }
+
+
+        public enum EUserType
+        {
+            User=0,
+            MedicapsStudent=1,
+            SahityakMember=2,
+            Admin=3,
+        }
+
+    }
+
+    public class LoginModel
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
